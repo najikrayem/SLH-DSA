c30cece2a827c422731f4cdbcd7d6b2cefbb6dc8
Naji <krayemnaji@gmail.com>
2024-03-31 21:26:29 -0400

    ph

--- test/test_script.sh
+++ test/test_script.sh
@@ -6,7 +6,28 @@ build/test/slh_ds_test
 
 
 # Run basic sign verify test
-rm sec pub sig
-build/example/cli/GenerateKeys sec pub
-build/example/cli/SignMessage sec msg sig
-build/example/cli/VerifySignature pub msg sig
+mkdir -p build/test_temp
+cd build/test_temp
+cp ../../test/test_data/msg msg
+#rm sec pub sig
+echo
+echo "Generating keys..."
+../example/cli/GenerateKeys sec pub
+
+echo
+echo "Signing message..."
+../example/cli/SignMessage sec msg sig
+
+echo
+echo "Verifying Signature..."
+echo
+../example/cli/VerifySignature pub msg sig
+
+echo
+echo "------------------"
+echo "Testing IFS Verifier..."
+echo
+../example/qnx_ifs_verify_mount/IFSVerifyMount pub msg sig
+cd ../..
+
+
