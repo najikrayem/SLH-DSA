44766ce09b5598561e1765cf6b1715d6dae60134
najikrayem <krayemnaji@gmail.com>
2024-05-11 14:57:50 -0400

    Disclaimer

--- build_script.sh
+++ build_script.sh
@@ -1,12 +1,29 @@
-rm -rf build
 
-export PROCESSOR="aarch64le"
+# Check if the build directory exists. If it does, remove it
+if [ -d "build" ]; then
+    rm -rf build
+fi
 
-cmake -G "Unix Makefiles" -S . -B build -DCMAKE_TOOLCHAIN_FILE=qnx800_aarch64le.cmake
+# Check target architecture input argument. Should be either aarch64_unix, x86_64_unix, or aaarch64_qnx
+if [ "$1" == "aarch64_unix" ]; then
 
-#cmake -G "Unix Makefiles" -S . -B build -DCMAKE_TOOLCHAIN_FILE=linux_x86_64.cmake
+    export PROCESSOR="aarch64le"
+    cmake -G "Unix Makefiles" -S . -B build -DCMAKE_TOOLCHAIN_FILE=linux_arm.cmake
 
-#cmake -G "Unix Makefiles" -S . -B build -DCMAKE_TOOLCHAIN_FILE=linux_arm.cmake
+elif [ "$1" == "x86_64_unix" ]; then
+    
+    export PROCESSOR="x86_64"
+    cmake -G "Unix Makefiles" -S . -B build -DCMAKE_TOOLCHAIN_FILE=linux_x86_64.cmake
+
+elif [ "$1" == "aarch64_qnx" ]; then
+    
+    export PROCESSOR="aarch64le"
+    cmake -G "Unix Makefiles" -S . -B build -DCMAKE_TOOLCHAIN_FILE=qnx800_aarch64le.cmake
+
+else
+    echo "Invalid target architecture. Please use either aarch64_unix, x86_64_unix, or aarch64_qnx"
+    exit 1
+fi
 
 cd build
 make verbose=3
---
+++ test/test_data/sample_ifs.bf
@@ -0,0 +1,4 @@
+#Build file for a sample secondary IFS
+#It contains the `msg` file which is used to verify the mount of the IFS
+
+example/qnx_ifs_verify_mount/msg
